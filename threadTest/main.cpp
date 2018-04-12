40a3c5295a2533bca85bd11e4b0f1d7654426cdf
Eduardo Cardoso Fernandes Neto <eduardo.cardoso10@gmail.com>
2018-04-11 21:58:35 -0300

    v0.1 - Add Test Project
    Adicionando Projeto CodeBlocos de test, feito na ultima aula(10/04/18).

---
+++ threadTest/main.cpp
@@ -0,0 +1,42 @@
+// thread example
+#include <iostream>       // std::cout
+#include <thread>         // std::thread
+#include <mutex>
+
+std::mutex t_mutex;
+
+void foo()
+{
+  for(int i=65;i<80;i++)
+  {
+     std::lock_guard<std::mutex> guard(t_mutex);
+     std::cout << "AAAAA" << std::endl;
+  }
+}
+
+void bar()
+{
+    for(int i=97;i<112;i++)
+  {
+        std::lock_guard<std::mutex> guard(t_mutex);
+        std::cout << "aa" << std::endl;
+  }
+
+}
+
+int main()
+{
+
+  std::thread first (foo);     // spawn new thread that calls foo()
+  std::thread second (bar);  // spawn new thread that calls bar(0)
+
+  std::cout << "main, foo and bar now execute concurrently...\n";
+
+  // synchronize threads:
+  first.join();                // pauses until first finishes
+  second.join();               // pauses until second finishes
+
+  std::cout << "\nfoo and bar completed.\n";
+
+  return 0;
+}
