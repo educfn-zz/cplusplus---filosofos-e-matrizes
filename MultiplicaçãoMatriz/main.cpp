c03a86c2df4277e7c3920721a1172637d35d773a
Eduardo Cardoso Fernandes Neto <eduardo.cardoso10@gmail.com>
2018-04-25 19:16:01 -0300

    v2.2.1 - terminado funcao criarMatriz
    Terminado a criacao da função criarMatriz(), contudo ainda é necessario criar a função exibirMatriz() para verificar seu funcionamento.

--- MultiplicaçãoMatriz/main.cpp
+++ MultiplicaçãoMatriz/main.cpp
@@ -37,21 +37,41 @@ public:
         return matriz2;
     }
 
-    void criarMatriz(int tam_x,int tam_y)
+    ptrMatriz criarMatriz(int tam_x,int tam_y)
     {
-        srand(time(NULL));
+        srand(time(NULL));//Semente de tempo para ser usada na geracao de valores aleatorios(rand()).
 
-        ptrMatriz p;//vai receber a referencia do ponteiro [0][0] da matriz.
-        if(tam_x!=0 && tam_y!=0)
+        ptrMatriz p=nullptr;//vai receber a referencia do ponteiro [0][0] da matriz.
+
+        if(tam_x>0 && tam_y>0)
         {
+            ptrMatriz aux_p = nullptr;//Ira segurar ate o final da criacao da matriz a referencia para o ponteiro [0][0].
+            //Pois caso ocorra alguma falha que termine abruptamente com a criada da matriz o ponteiro principal permanecera
+            //nulo indicando que ocorreu falha na geracao da matriz.
             ptrMatriz q = nullptr;//ponteiro que ira realizar toda a criacao da matriz.
-            ptrMatriz aux = nullptr;//ponteiro para o no do inicio da linha.
-            ptrMatriz aux_l = nullptr;//ponteiro para o no anterior do ponteiro `q`.
+            ptrMatriz aux = nullptr;//ponteiro para o noh do inicio da linha.
+            ptrMatriz aux_l = nullptr;//ponteiro para o noh anterior do ponteiro `q`.
             for(int i=0;i<tam_y;i++)
             {
                 for(int j=0;j<tam_x;j++)
                 {
-                    if(i!=0 && j==0)
+                    if(i!=0 && j!=0)
+                    {
+                        if((q = (ptrMatriz) malloc(sizeof(no_matriz)))== nullptr)
+                        {
+                            std::cout << "Malloc erro: Erro de alocacao de memoria!";
+                            exit(-1);
+                        }
+
+                        q->ant=aux_l;
+                        aux_l->prox=q;
+                        aux_l = q;
+                        q->prox=nullptr;
+                        q->emb=nullptr;
+                        q->valor= rand() % 999 + 0;
+                        q = nullptr;
+                    }
+                    else if(i!=0 && j==0)
                     {
                         if((q = (ptrMatriz) malloc(sizeof(no_matriz)))== nullptr)
                         {
@@ -63,28 +83,32 @@ public:
                         aux = aux_l = q;
                         q->ant=nullptr;
                         q->prox=nullptr;
+                        q->emb=nullptr;
                         q->valor= rand() % 999 + 0;
+                        q=nullptr;
                     }
                     else if(i==0 && j==0)
                     {
-                        if((p = (ptrMatriz) malloc(sizeof(no_matriz)))== nullptr)
+                        if((aux_p = (ptrMatriz) malloc(sizeof(no_matriz)))== nullptr)
                         {
                             std::cout << "Malloc erro: Erro de alocacao de memoria!";
                             exit(-1);
                         }
 
-                        aux = aux_l = p;
-                        p->ant=nullptr;
-                        p->emb=nullptr;
-                        p->prox=nullptr;
-                        p->valor= rand() % 999 + 0;
+                        aux = aux_l = aux_p;
+                        aux_p->ant=nullptr;
+                        aux_p->emb=nullptr;
+                        aux_p->prox=nullptr;
+                        aux_p->valor= rand() % 999 + 0;
                     }
 
-                }
-            }
-        }
+                }//Fim da interacao da linha
+            }//Fim da interacao coluna
+            p = aux_p;
+        }//Fim da criacao da matriz
+        return p;
+    }//Fim da funcao 'criarMatriz()'.
 
-    }
 };
 
 int main()
