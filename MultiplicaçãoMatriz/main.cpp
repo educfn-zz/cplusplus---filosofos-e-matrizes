d062465cb6e87bebcb1764f32430bb56ce428571
Eduardo Cardoso Fernandes Neto <eduardo.cardoso10@gmail.com>
2018-04-25 18:41:49 -0300

    v2.2 - MatrizMultiplicacao
    Mudanças desenvolvimento da função criarMatriz(), ainda não acabada.

--- MultiplicaçãoMatriz/main.cpp
+++ MultiplicaçãoMatriz/main.cpp
@@ -1,6 +1,8 @@
+//ALUNO: EDUARDO CARDOSO FERNANDS NETO - 5 SEMESTRE - NUMERO CHAMADA:9197350
 #include <iostream>
 #include <cstdlib>
 #include <cstdbool>
+#include <ctime>
 
 typedef struct no_matriz * ptrMatriz;
 
@@ -15,8 +17,8 @@ struct no_matriz
 class Matriz
 {
 private:
-    float* matriz1;
-    float* matriz2;
+    ptrMatriz matriz1;
+    ptrMatriz matriz2;
 
 public:
     Matriz()
@@ -25,20 +27,59 @@ public:
         matriz2=nullptr;
     }
 
+    ptrMatriz getPtrMatriz1()
+    {
+        return matriz1;
+    }
+
+    ptrMatriz getPtrMatriz2()
+    {
+        return matriz2;
+    }
+
     void criarMatriz(int tam_x,int tam_y)
     {
-        float* p;//vai receber a referencia do ponteiro [0][0].
+        srand(time(NULL));
+
+        ptrMatriz p;//vai receber a referencia do ponteiro [0][0] da matriz.
         if(tam_x!=0 && tam_y!=0)
         {
+            ptrMatriz q = nullptr;//ponteiro que ira realizar toda a criacao da matriz.
+            ptrMatriz aux = nullptr;//ponteiro para o no do inicio da linha.
+            ptrMatriz aux_l = nullptr;//ponteiro para o no anterior do ponteiro `q`.
             for(int i=0;i<tam_y;i++)
             {
                 for(int j=0;j<tam_x;j++)
                 {
-                    if((p = (float*) malloc(sizeof(float)))== nullptr)
+                    if(i!=0 && j==0)
                     {
-                        std::cout << "Malloc erro: Erro de alocacao de memoria!";
-                        exit(-1);
+                        if((q = (ptrMatriz) malloc(sizeof(no_matriz)))== nullptr)
+                        {
+                            std::cout << "Malloc erro: Erro de alocacao de memoria!";
+                            exit(-1);
+                        }
+
+                        aux->emb = q;
+                        aux = aux_l = q;
+                        q->ant=nullptr;
+                        q->prox=nullptr;
+                        q->valor= rand() % 999 + 0;
                     }
+                    else if(i==0 && j==0)
+                    {
+                        if((p = (ptrMatriz) malloc(sizeof(no_matriz)))== nullptr)
+                        {
+                            std::cout << "Malloc erro: Erro de alocacao de memoria!";
+                            exit(-1);
+                        }
+
+                        aux = aux_l = p;
+                        p->ant=nullptr;
+                        p->emb=nullptr;
+                        p->prox=nullptr;
+                        p->valor= rand() % 999 + 0;
+                    }
+
                 }
             }
         }
@@ -48,23 +89,8 @@ public:
 
 int main()
 {
-    Matriz m;
-    std::cout << "Hello world!" << std::endl;
-    //m = new Matriz::Matriz();
+    Matriz m = Matriz();
+    std::cout << m.getPtrMatriz1() << std::endl;
     m.criarMatriz(0,0);
     return 0;
 }
-
-void exibirMatriz(ptrMatriz m)
-{
-     ptrMatriz p = m;
-     while(1)
-     {
-
-          std::cout << "\t[" << p
-          << "]";
-     }
-}
-
-
-
